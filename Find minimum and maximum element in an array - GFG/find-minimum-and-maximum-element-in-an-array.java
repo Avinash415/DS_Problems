20e564450c777308867cba43faceab36ddb4c353
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-09-02 21:55:20 +0530

    Added solution - LeetHub

---
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            Pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+ class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    
+    
+    static Pair getMinMax(long arr[], long n)  
+    {
+        //Write your code here
+       long max=Long.MIN_VALUE;
+       long min=Long.MAX_VALUE;
+       for(int i=0; i<n; i++)
+       {
+           //max..
+           if(arr[i]>max)
+           {
+               max=arr[i];
+           }
+           //min..
+           if(arr[i]<min)
+           {
+               min=arr[i];
+           }
+       }
+       Pair obj=new Pair(min, max);
+       return obj;
+        
+       
+    }
+}
+
+
