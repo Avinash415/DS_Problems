3b6b3013b740f3819a04c49fb7d4c83a81957097
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-09-23 22:40:21 +0530

    Added solution - LeetHub

--- Equilibrium Point - GFG/equilibrium-point.java
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -42,24 +42,38 @@ class Solution {
 
         // Your code here
         // 1st approach
-        if(n==1)  return 1;
-        long sum=0;
-        for(long x: arr)
-        {
-            sum+=x;
-        }
+        // if(n==1)  return 1;
+        // long sum=0;
+        // for(long x: arr)
+        // {
+        //     sum+=x;
+        // }
         
-        long sum1=arr[0];
-        sum=sum-arr[0];
+        // long sum1=arr[0];
+        // sum=sum-arr[0];
         
-        for(int i=1; i<n; i++)
-        {
-            sum=sum-arr[i];
-            if(sum==sum1)   return i+1;
-            sum1= sum1+arr[i];
+        // for(int i=1; i<n; i++)
+        // {
+        //     sum=sum-arr[i];
+        //     if(sum==sum1)   return i+1;
+        //     sum1= sum1+arr[i];
             
-        }
+        // }
         
+        // return -1;
+        
+        // 2nd aproach
+        
+        if(n==1)  return 1;
+        long s1=arr[0], s2=arr[n-1];
+        
+        int i=1, j=n-2;
+        while(i<=j)
+        {
+            if(s1==s2 && i==j)  return i+1;
+            if(s1<s2)  s1+=arr[i++];
+            else s2+=arr[j--];
+        }
         return -1;
     }
 }
