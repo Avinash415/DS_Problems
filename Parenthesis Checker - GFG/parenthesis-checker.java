40d45cb3ff4231d8c63bcfac32eb4fd4825b0f80
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-10-01 16:28:43 +0530

    Added solution - LeetHub

---
+++ Parenthesis Checker - GFG/parenthesis-checker.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+      Stack<Character> s=new Stack<>();
+
+        for(int i=0; i<x.length(); i++)
+        {
+            char ch=x.charAt(i);
+
+            if(ch=='(' || ch=='{' || ch=='[' )  // opening
+            {
+                s.push(ch);
+            }
+            else{
+                //closing..
+                if(s.isEmpty())
+                {
+                    return false;
+                }
+                if((s.peek()=='(' && ch==')') || (s.peek()=='{' && ch=='}')
+                    || (s.peek()=='[' && ch==']'))
+                    {
+                        s.pop();
+                    }
+                    else
+                   {
+                    return false;
+                   }
+            }
+        }
+        if(s.isEmpty())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+     }
+     
+    }
+    
+
