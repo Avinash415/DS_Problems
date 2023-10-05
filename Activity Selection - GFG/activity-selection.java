e894ca6691da94f7bd05352d23e5078460b81f01
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-10-06 02:53:26 +0530

    Added solution - LeetHub

---
+++ Activity Selection - GFG/activity-selection.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*; 
+import java.util.*; 
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) throws IOException  
+    {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		
+		while(t-- > 0)
+		{
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    //size of array
+		    int n = Integer.parseInt(inputLine[0]);
+		    int start[] = new int[n];
+		    int end[] = new int[n];
+		    
+		    //adding elements to arrays start and end
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i = 0; i < n; i++)
+		     start[i] = Integer.parseInt(inputLine[i]);
+		    
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i= 0; i < n; i++)
+		      end[i] = Integer.parseInt(inputLine[i]);
+		    
+		    //function call
+		    System.out.println(new Solution().activitySelection(start, end, n));
+		}
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    public static int activitySelection(int start[], int end[], int n)
+    {
+        // add your code here
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i+1;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+        int maxAct=0;
+        maxAct = 1;
+        
+         int lastEnd = activities[0][2];
+        for (int i = 1; i < end.length; i++) {
+            if (activities[i][1] > lastEnd) {
+                // activity select..
+                maxAct++;
+            
+                lastEnd = activities[i][2];
+            }
+        }
+        
+        return maxAct;
+    }
+}
