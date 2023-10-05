8969b8f63799910e5f34670a2c9a371172ab35f7
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-10-05 19:22:02 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean vis[]=new boolean[V];
+        
+        for(int i=0; i<V; i++)
+        {
+            if(!vis[i])
+            {
+                if(dfs(i, adj, vis, -1))  return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean dfs(int v, ArrayList<ArrayList<Integer>> adj, boolean vis[], int parent)
+    {
+        vis[v]=true;
+        
+        for(Integer neighbor: adj.get(v))
+        {
+            if(!vis[neighbor])
+            {
+                if(dfs(neighbor, adj, vis, v))
+                return true;
+            }
+            else if(parent != neighbor)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
