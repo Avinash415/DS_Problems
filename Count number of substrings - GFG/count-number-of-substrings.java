de4b8c58c4aca64060265bd0fbb0e8b1303db6aa
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2023-10-05 15:35:49 +0530

    Added solution - LeetHub

---
+++ Count number of substrings - GFG/count-number-of-substrings.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            int k = sc.nextInt();
+    		System.out.println (new Solution().substrCount (s, k));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    long substrCount (String s, int k) {
+        // your code here
+        return atmost(s, k)-atmost(s, k-1);
+    }
+    
+    long atmost(String s, int k)
+    {
+        int arr[]= new int[26];
+        
+        int dist=0, left=0;
+        long res=0;
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            arr[s.charAt(i)-'a']++;
+            if(arr[s.charAt(i)-'a']==1)   dist++;
+            
+            while(dist>k)
+            {
+               arr[s.charAt(left)-'a']--;
+               if(arr[s.charAt(left)-'a']==0)   dist--;
+               left++;
+            }
+          res=res+(i-left+1);
+        }
+        
+       return res; 
+    }
+}
